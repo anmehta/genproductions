ef4ee85b50b39eac5a7ced8cae9092573bce59e8
Giacomo Boldrini <gboldrin@login-el7.uscms.org>
2022-03-20 08:16:25 -0500

    Modified becuase now git is installed on worker nodes /cvmfs/oasis.opensciencegrid.org/mis/osg-wn-client/3.5/3.5.58-1/el7-x86_64/usr/bin/git from command -v git

--- bin/MadGraph5_aMCatNLO/gridpack_generation_EFT.sh
+++ bin/MadGraph5_aMCatNLO/gridpack_generation_EFT.sh
@@ -712,7 +712,7 @@ fi
 helpers_dir=${PRODHOME%genproductions*}/genproductions/Utilities
 helpers_file=${helpers_dir}/gridpack_helpers.sh
 if [ ! -f "$helpers_file" ]; then
-  if ! [ -x "$(command -v git)" ]; then
+  if  [ -x "$(command -v git)" ]; then
     helpers_dir=${PRODHOME}/Utilities
   else
     helpers_dir=$(git rev-parse --show-toplevel)/bin/MadGraph5_aMCatNLO/Utilities
