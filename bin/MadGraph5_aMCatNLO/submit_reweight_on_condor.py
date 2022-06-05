f2871628c62f7f36ec12d44666927ffdc52035fc
GiacomoBoldrini <g.boldrini2@campus.unimib.it>
2022-06-05 05:44:56 -0500

    Submit reweight on condor after integrate step

--- bin/MadGraph5_aMCatNLO/submit_reweight_on_condor.py
+++ bin/MadGraph5_aMCatNLO/submit_reweight_on_condor.py
@@ -2,6 +2,57 @@ import os
 import sys
 import argparse
 import copy
+import subprocess
+import time
+from glob import glob
+
+
+def precompile_rwgt_dir(madevent_path):
+    print("Precompiling reweighting modules")
+
+    orig = os.getcwd()
+    os.chdir(madevent_path)
+    
+    for file in glob("rwgt/*/*/SubProcesses/P*"):
+        print ("Compiling subprocess " + file)
+        os.chdir(file)
+        for i in [2, 3]:
+            os.environ['MENUM'] = str(i)
+            out = os.system("MENUM={i} make matrix{i}py.so >& /dev/null".format(i=i))   
+            print("Library MENUM={} compiled with status {}".format(i, out))
+        os.chdir(madevent_path)
+    
+    os.chdir(orig)
+    
+    return 
+
+def make_tarball(WORKDIR, iscmsconnect, PRODHOME, CARDSDIR, CARDNAME, scram_arch, cmssw_version):
+    print("---> Creating tarball")
+    os.chdir("{WORKDIR}/gridpack".format(WORKDIR=WORKDIR))
+
+    if  iscmsconnect:  os.environ['XZ_OPT'] = "--lzma2=preset=2,dict=256MiB"
+    else:              os.environ['XZ_OPT'] = "--lzma2=preset=9,dict=512MiB"
+
+    if os.path.isdir("InputCards"): 
+      os.system("rm -rf InputCards")
+
+    mkdir("InputCards")
+
+    os.system("cp {CARDSDIR}/{CARDNAME}*.* InputCards".format(CARDSDIR=CARDSDIR,CARDNAME=CARDNAME ))
+
+    EXTRA_TAR_ARGS=""
+    if os.path.isfile("{CARDSDIR}/{CARDNAME}_externaltarball.dat".format(CARDSDIR=CARDSDIR,CARDNAME=CARDNAME )):
+        EXTRA_TAR_ARGS="external_tarball header_for_madspin.txt "
+
+    ### include merge.pl script for LO event merging 
+    if os.path.isfile("merge.pl"):
+        EXTRA_TAR_ARGS+="merge.pl "
+    os.system("XZ_OPT=\"$XZ_OPT\" tar -cJpsf {PRODHOME}/{CARDNAME}_{scram_arch}_{cmssw_version}_tarball.tar.xz mgbasedir process runcmsgrid.sh InputCards {EXTRA_TAR_ARGS}".format( PRODHOME=PRODHOME, CARDNAME=CARDNAME, scram_arch=scram_arch, cmssw_version=cmssw_version, EXTRA_TAR_ARGS=EXTRA_TAR_ARGS ))
+    print("Gridpack created successfully at {PRODHOME}/{CARDNAME}_{scram_arch}_{cmssw_version}_tarball.tar.xz".format(PRODHOME=PRODHOME, CARDNAME=CARDNAME, scram_arch=scram_arch, cmssw_version=cmssw_version))
+    print("End of job")
+
+    return
+
 
 def mkdir(path):
     try:
@@ -29,11 +80,20 @@ fi
 ls
 tar xfz "{input_files}"
 
-mv {exec_name}.dat ${{condor_scratch}}/{card_name}/{card_name}_gridpack/work/process/madevent/Cards/reweight_card.dat
+mv condor_sub/{exec_name}.dat ${{condor_scratch}}/{card_name}/{card_name}_gridpack/work/process/madevent/Cards/reweight_card.dat
+
+echo "mg5_path = ${{condor_scratch}}/{card_name}/{card_name}_gridpack/work/MG5_aMC_v2_6_5" >> ${{condor_scratch}}/{card_name}/{card_name}_gridpack/work/process/madevent/Cards/me5_configuration.txt
+#echo "cluster_temp_path = None" >> ./madevent/Cards/me5_configuration.txt
+#echo "run_mode = 0" >> ./madevent/Cards/me5_configuration.txt 
 
 # Setup CMS framework
 export VO_CMS_SW_DIR=/cvmfs/cms.cern.ch
 source $VO_CMS_SW_DIR/cmsset_default.sh
+
+# For modified gridpack generation script set this variable to exit after reweight and not create the tarball
+export REWEIGHT_ON_CONDOR=true
+
+
 # Purdue wokaround
 unset CXX CC FC
 # Run
@@ -45,6 +105,7 @@ if [ $exitcode -ne 0 ]; then
 fi
 # Pack output and condor scratch dir info
 cd "${{condor_scratch}}/{card_name}/{card_name}_gridpack/work/process/madevent"
+ls
 XZ_OPT="--lzma2=preset=9,dict=512MiB" tar -cJpsf "${{condor_scratch}}/{sandbox_output}" "rwgt"
 
 # Stage-out sandbox
@@ -77,8 +138,8 @@ Output = condor_log/job.out.$(Cluster)-$(Process)
 Log = condor_log/job.log.$(Cluster) 
 
 transfer_input_files = input_reweight_{card_name}.tar.gz, gridpack_generation_EFT.sh, /usr/bin/unzip
-#transfer_output_files = {card_name}.log, {exec_name}_output.tar.xz
-#transfer_output_remaps = \"{card_name}.log = {exec_name}.log\"
+transfer_output_files = {card_name}.log, {exec_name}_output.tar.xz
+transfer_output_remaps = \"{card_name}.log = {exec_name}.log\"
 +WantIOProxy=true
 +IsGridpack=true
 +GridpackCard = \"{card_name}\"
@@ -111,10 +172,6 @@ def write_rew_dict(rew_dict, output_folder, cardname, cardpath):
 
         #writing .sh
         write_sh("rwgt_{}".format(key), cardname, cardpath, output_folder="")
-        #f = open(output_folder + "rwgt_{}.sh".format(key), "w")
-        #for line in rew_dict[key]:
-        #    f.write(line)
-        #f.close() 
 
     return 
 
@@ -175,6 +232,8 @@ if __name__ == "__main__":
     parser.add_argument('-cn', '--cardname',     dest='cardname',     help='The name of the cards, <name>_proc_card.dat', required = True)
     parser.add_argument('-cp', '--cardpath',     dest='cardpath',     help='The path to the cards directory', required = True)
     parser.add_argument('-sf', '--subfolder',     dest='subfolder',     help='The path to the folder where .jid, exec and reweight cards will be saved', required = False, default="condor_sub")
+    parser.add_argument('-is5f', '--is5FlavorScheme',     dest='is5FlavorScheme',     help='Is the gridpack intended for 5fs? Default is true', required = False, default=True, action="store_false")
+    parser.add_argument('-iscmsc', '--iscmsconnect',     dest='iscmsconnect',     help='Are you working on cmsconnect? Default is true', required = False, default=True, action="store_false")
 
 
     args = parser.parse_args()
@@ -188,6 +247,15 @@ if __name__ == "__main__":
     if not os.path.isfile(args.cardpath + "/" + args.cardname + "_reweight_card.dat"): sys.exit("[ERROR]  reweight card does not exist in {}".format(args.cardpath))
     
 
+    PRODHOME = os.getcwd()
+    CARDSDIR=os.path.join(PRODHOME, args.cardpath)
+    helpers_dir=os.path.join(PRODHOME, "Utilities")
+    WORKDIR = os.path.join(PRODHOME, args.cardname, args.cardname+"_gridpack", "work") 
+    genp_name = PRODHOME.split("/bin/MadGraph5_aMCatNLO")[0].split("/")[-1]
+    script_dir=os.path.join(PRODHOME.split(genp_name)[0], "genproductions/", "Utilities/scripts")
+    cmssw_version="CMSSW_10_6_19"
+    scram_arch="slc7_amd64_gcc700"
+    MGBASEDIRORIG = "MG5_aMC_v2_6_5"
     patches_directory="./patches"
     utilities_dir="./Utilities"
     plugin_directory="./PLUGIN"
@@ -201,24 +269,115 @@ if __name__ == "__main__":
     # write the separate reweight point in a file
     write_rew_dict(rd, args.subfolder, args.cardname, args.cardpath)
 
-
+    
     if os.path.isfile(input_files) or os.path.isdir(input_files): 
         print("Tarball allready present. reusing")
     else:
         print("tar -zchvf \"{input_files}\" {rwgt_cards} \"{card_name}\" \"{card_dir}\" \"{patches_directory}\" \"{utilities_dir}\" \"{plugin_directory}\"".format(input_files=input_files, rwgt_cards=" ".join(["\"" + args.subfolder+ "/" "rwgt_" + str(key) + ".dat\"" for key in rd.keys()] ), card_name=args.cardname, card_dir=args.cardpath, patches_directory=patches_directory, utilities_dir=utilities_dir, plugin_directory=plugin_directory))
         os.system("tar -zchvf \"{input_files}\" {rwgt_cards} \"{card_name}\" \"{card_dir}\" \"{patches_directory}\" \"{utilities_dir}\" \"{plugin_directory}\"".format(input_files=input_files, rwgt_cards=" ".join(["\"" + args.subfolder+ "/" "rwgt_" + str(key) + ".dat\"" for key in rd.keys()] ), card_name=args.cardname, card_dir=args.cardpath, patches_directory=patches_directory, utilities_dir=utilities_dir, plugin_directory=plugin_directory))
 
-    #os.system("mv {} {}".format(input_files, args.subfolder))
-    #os.system("cp {} {}".format("gridpack_generation_EFT.sh", args.subfolder))
-    #os.chdir(args.subfolder)
+    
     mkdir("condor_log")
 
     print(">> Submitting REWEIGHT condor job and wait")
 
+
     for key in rd.keys():
         os.system("condor_submit \"{}\" | tail -n1 | rev | cut -d' ' -f1 | rev".format("rwgt_" + str(key) + ".jdl"))
 
+    #colllecting jobs ids
+    out = subprocess.Popen(["condor_q", "-format",  "%d.", "ClusterId", "-format",  "%d\n",  "ProcId"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    stdout,stderr = out.communicate()
+    all_procs = stdout.split("\n")[:-1]
+    this_procs = all_procs[:len(rd.keys())] # the proc we submitted hopefully are the last ones
+    print(all_procs)
+
+    while(any(i in all_procs for i in this_procs)):
+
+        #querying again the condor scheduler
+        out = subprocess.Popen(["condor_q", "-format",  "%d.", "ClusterId", "-format",  "%d\n",  "ProcId"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        stdout,stderr = out.communicate()
+        all_procs = stdout.split("\n")[:-1]
+        print(all_procs)
+        time.sleep(5)
+    
+    print("---> ALL Jobs Finished")
+
+    not_ok = []
+    #check for all the outputs
+    for key in rd.keys():
+        if not os.path.isfile("rwgt_" + str(key) + "_output.tar.xz"): 
+            print("[ERROR] No output found for rwgt_" + str(key) + "_output.tar.xz")
+            not_ok.append("rwgt_" + str(key))
+    
+    if len(not_ok) > 0: sys.exit(0)
+    
+
+    #create rwgt dir if not present
+    if not os.path.isdir(args.cardname + "/" + args.cardname + "_gridpack/work/process/madevent/rwgt"):
+        mkdir(args.cardname + "/" + args.cardname + "_gridpack/work/process/madevent/rwgt")
+    
+    for key in rd.keys():
+        #os.system("rm rwgt_" + str(key) + ".sh")
+        #os.system("rm rwgt_" + str(key) + ".jdl")
+
+        os.system("tar axvf rwgt_" + str(key) + "_output.tar.xz")
+
+        if not os.path.isdir(args.cardname + "/" + args.cardname + "_gridpack/work/process/madevent/rwgt"):
+            os.mkdir(args.cardname + "/" + args.cardname + "_gridpack/work/process/madevent/rwgt")
+        os.system("mv rwgt/* " + args.cardname + "/" + args.cardname + "_gridpack/work/process/madevent/rwgt")
+        os.system("rm -rf rwgt")
+        #os.system("rm rwgt_" + str(key) + "_output.tar.xz")
+
+    # compiling reweight dirs
+    precompile_rwgt_dir(os.getcwd() + "/" + args.cardname + "/" + args.cardname + "_gridpack/work/process/madevent")
+
+    
+    #############################################
+
+    print("---> Preparing final gridpack")
+    os.chdir(args.cardname + "/" + args.cardname + "_gridpack/work/process")
+    os.system("echo \"mg5_path = ../../mgbasedir\" >> ./madevent/Cards/me5_configuration.txt")
+    os.system("echo \"cluster_temp_path = None\" >> ./madevent/Cards/me5_configuration.txt")
+    os.system("echo \"run_mode = 0\" >> ./madevent/Cards/me5_configuration.txt")
+
+    os.chdir(WORKDIR)
 
 
+
+    if os.path.isdir("gridpack"):
+        os.syste("rm -rf gridpack")
+
+    mkdir("gridpack")
+    os.system("cp -r process gridpack/process")
+    os.system("cp -a {}/ gridpack/mgbasedir".format(MGBASEDIRORIG))
+        
+
+    os.chdir("gridpack")
+    os.system("cp {}/runcmsgrid_LO.sh ./runcmsgrid.sh".format(PRODHOME))
+
+    os.system("sed -i s/SCRAM_ARCH_VERSION_REPLACE/{}/g runcmsgrid.sh".format(scram_arch))
+    os.system("sed -i s/CMSSW_VERSION_REPLACE/{}/g runcmsgrid.sh".format(cmssw_version))
+    
+    pdfExtraArgs=""
+
+    if args.is5FlavorScheme:
+      pdfExtraArgs+="--is5FlavorScheme"
+
+    out = subprocess.Popen(["python", "{}/getMG5_aMC_PDFInputs.py".format(script_dir),  "-f",  "systematics", "-c",  "2017", "{}".format(pdfExtraArgs)], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    pdfSysArgs,stderr = out.communicate()
+
+    os.system("sed -i s/PDF_SETS_REPLACE/{pdfSysArgs}/g runcmsgrid.sh".format(pdfSysArgs=pdfSysArgs[:-1]))
+
+    #clean unneeded files for generation
+    os.system("{helpers_dir}/cleangridmore.sh".format(helpers_dir=helpers_dir))
+
+    # copy merge.pl from Utilities to allow merging LO events
+    os.chdir("{}/gridpack".format(WORKDIR))
+    os.system("cp {}/Utilities/merge.pl .".format(PRODHOME)) 
+
+
+    #Finishing the gridpack
+    make_tarball(WORKDIR, args.iscmsconnect, PRODHOME, CARDSDIR, args.cardname, scram_arch, cmssw_version)
 
     print("--> Done <---")
